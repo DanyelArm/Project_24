b0f42a1a30f060c586d7ed27682565b63f530848
DanyelArm <daniel.armasari@gmail.com>
2023-12-28 15:29:16 +0200

    Third commit
    Adaugarea unui meniu de optiuni la fel cu  functiile de generarea si pozitie

--- Project_24_AD.cpp
+++ Project_24_AD.cpp
@@ -4,12 +4,33 @@ using namespace std;
 
 int N, T[100][100];
 
-void afisare()
-{
-    int i, j;
+void generare()
+{   
+    int i,j;
+    cout<<"Da-ti N: ";
+    cin>>N;
     for(i=0;i<N;i++)
     {
-        for(j=0;j<N;j++)
+        for (j=0;j<N;j++)
+        {
+            T[i][j]=0;
+            cout<<T[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pozitia()
+{   
+    int i,j;
+    do{
+        cout<<"Alege pozitia calului pe tabla(i,j mai mici decat N): ";
+        cin>>i>>j;
+    }while(i>=N || j>=N);
+    T[i][j]=2;
+    for(i=0;i<N;i++)
+    {
+        for (j=0;j<N;j++)
         {
             cout<<T[i][j]<<" ";
         }
@@ -19,8 +40,29 @@ void afisare()
 
 int main()
 {
-    cout<<"Da-ti N: ";
-    cin>>N;
-    afisare();
+    int opt;
+    do{
+        cout<<"-----Meniu-----"<<endl;
+        cout<<"0. Iesire."<<endl;
+        cout<<"1. Genenrarea de Tabla initiala."<<endl;
+        cout<<"2. Alegerea de pozitie."<<endl;
+        cout<<"Alege optiunea: ";
+        cin>>opt;
+        switch (opt)
+        {
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            generare();
+            break;
+        case 2:
+            pozitia();
+            break;
+        default:
+            cout<<"Opiunea aleasa este gresita.";
+            break;
+        }
+    }while(opt!=0);
     return 0;
 }
