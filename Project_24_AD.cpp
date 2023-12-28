0986a0aee9114e03c9140d68543893dae475698d
DanyelArm <daniel.armasari@gmail.com>
2023-12-28 14:06:35 +0200

    Second commit
    Start-ul de code

--- Project_24_AD.cpp
+++ Project_24_AD.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+int N, T[100][100];
+
+void afisare()
+{
+    int i, j;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            cout<<T[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
-    cout<<"Hello";
+    cout<<"Da-ti N: ";
+    cin>>N;
+    afisare();
     return 0;
 }
