6b673aa880f50cc70ca2ffa7397f2c83ffd41adb
DanyelArm <daniel.armasari@gmail.com>
2023-12-29 13:34:33 +0200

    Fourth commit
    Schimbari in functia pozitia si aduagarea unei functii de verificare la fel a unei functii de rezolvare a turului de cal

--- Project_24_AD.cpp
+++ Project_24_AD.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int N, T[100][100];
+const int miscari[8][2] = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+int countMiscari = 1;
 
 void generare()
 {   
@@ -20,14 +22,13 @@ void generare()
     }
 }
 
-void pozitia()
+void pozitia(int i, int j)
 {   
-    int i,j;
     do{
         cout<<"Alege pozitia calului pe tabla(i,j mai mici decat N): ";
         cin>>i>>j;
     }while(i>=N || j>=N);
-    T[i][j]=2;
+    T[i][j]=1;
     for(i=0;i<N;i++)
     {
         for (j=0;j<N;j++)
@@ -38,9 +39,47 @@ void pozitia()
     }
 }
 
+int verifica(int x, int y)
+{
+    if(x >= 0 && x < N && y >= 0 && y < N && T[x][y] == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int solve(int x, int y, int movei)
+{
+    if (movei == N * N)
+        return 1;
+
+    int k, next_x, next_y;
+    for (k = 0; k < 8; k++)
+    {
+        next_x = x + miscari[k][0];
+        next_y = y + miscari[k][1];
+
+        if (verifica(next_x, next_y))
+        {
+            T[next_x][next_y] = ++countMiscari;
+
+            if (solve(next_x, next_y, movei + 1) == 1)
+                return 1;
+            else
+                T[next_x][next_y] = 0;
+        }
+    }
+
+    return 0;
+}
+
+/*void afisare()
+{
+
+}
+*/
 int main()
 {
-    int opt;
+    int opt, i, j;
     do{
         cout<<"-----Meniu-----"<<endl;
         cout<<"0. Iesire."<<endl;
@@ -57,10 +96,10 @@ int main()
             generare();
             break;
         case 2:
-            pozitia();
+            pozitia(i,j);
             break;
         default:
-            cout<<"Opiunea aleasa este gresita.";
+            cout<<"Opiunea aleasa este gresita."<<endl;
             break;
         }
     }while(opt!=0);
